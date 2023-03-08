6a2487d0a4b1dddeaeb004c94303f808dd985c54
nguyentrungtin <nguyentrungtin.tech@gmail.com>
2023-03-08 14:20:59 +0700

    Tin Update Scheduling

--- src/scheduling/scheduling.service.ts
+++ src/scheduling/scheduling.service.ts
@@ -1,33 +1,20 @@
-import * as axios from 'axios';
 import { Body, Injectable, ForbiddenException } from '@nestjs/common';
 import { CreateSchedulingDto } from './dto/create-scheduling.dto';
 import { UpdateSchedulingDto } from './dto/update-scheduling.dto';
 import { Schedule } from 'src/utils/schemas';
 import { map, catchError, lastValueFrom, async } from 'rxjs';
 import { Model } from 'mongoose';
-import {
-  Teacher,
-  Room,
-  Class,
-  StudentRegister,
-  Subject,
-} from 'src/utils/schemas';
+import { Class, StudentRegister, Subject } from 'src/utils/schemas';
 import { InjectModel } from '@nestjs/mongoose';
-import { RegisterClassModule } from './../register-class/register-class.module';
-import { RegisterClassService } from 'src/register-class/register-class.service';
-import { HttpModule, HttpService } from '@nestjs/axios';
-import { response } from 'express';
+import { HttpService } from '@nestjs/axios';
 @Injectable()
 export class SchedulingService {
   constructor(
     @InjectModel('Schedule') private readonly scheduleModel: Model<Schedule>,
     @InjectModel('Class') private readonly classModel: Model<Class>,
     @InjectModel('Subject') private readonly subjectModel: Model<Subject>,
-    @InjectModel('Teacher') private readonly teacherModel: Model<Teacher>,
-    @InjectModel('Room') private readonly roomModel: Model<Room>,
     @InjectModel('StudentRegister')
     private readonly studentRegisterModel: Model<StudentRegister>,
-    private readonly registerClassService: RegisterClassService,
     private readonly httpService: HttpService,
   ) {}
   async create(body: CreateSchedulingDto): Promise<Schedule> {
@@ -44,23 +31,15 @@ export class SchedulingService {
   }
 
   async schedulingTimeTable(major: string) {
-    const _subject = await this.subjectModel.find({ marjor_learn: major });
-
     const _class = await this.classModel
       .find({ status: false })
       // .populate('id_subject')
       .populate({ path: 'id_subject', match: { marjor_learn: major } })
       .populate('id_teacher')
       .populate('id_room');
-    const idList = _class.map((item) => {
-      return item._id;
-    });
-    const _studentRegister = await this.studentRegisterModel.find({
-      id_class: { $in: idList },
-    });
 
     const inputFinal = [];
-    let dataTeacher = _class.map((item) => {
+    const dataTeacher = _class.map((item) => {
       if (
         item.id_teacher == null ||
         item.id_subject == null ||
@@ -79,7 +58,7 @@ export class SchedulingService {
       inputFinal.push({ prof: teacherList });
       return teacherList;
     });
-    let dataSubject = _class.map((item) => {
+    const dataSubject = _class.map((item) => {
       if (
         item.id_teacher == null ||
         item.id_subject == null ||
@@ -87,8 +66,8 @@ export class SchedulingService {
       ) {
         return;
       }
-      const { class_name, id_subject } = item;
-      const { id_subject: subjectID, subject_name } = id_subject;
+      const { id_subject } = item;
+      const { subject_name } = id_subject;
       const subjectList = {
         name: subject_name,
         id: id_subject.id_subject.toString(),
@@ -97,7 +76,7 @@ export class SchedulingService {
       inputFinal.push({ course: subjectList });
       return subjectList;
     });
-    let dataRoom = _class.map((item) => {
+    const dataRoom = _class.map((item) => {
       const { id_room } = item;
 
       const { name_room, seats, lab } = id_room;
@@ -110,7 +89,7 @@ export class SchedulingService {
       inputFinal.push({ room: roomList });
       return roomList;
     });
-    let dataGroup = _class.map((item) => {
+    const dataGroup = _class.map((item) => {
       if (
         item.id_teacher == null ||
         item.id_subject == null ||
@@ -129,7 +108,7 @@ export class SchedulingService {
       return groupList;
     });
 
-    let dataClass = _class.map((item) => {
+    const dataClass = _class.map((item) => {
       if (
         item.id_teacher == null ||
         item.id_subject == null ||
@@ -137,13 +116,7 @@ export class SchedulingService {
       ) {
         return;
       }
-      const {
-        id_class: classID,
-        _id: mongo_class_id,
-        class_name,
-        id_teacher,
-        id_subject,
-      } = item;
+      const { _id: mongo_class_id, id_teacher, id_subject } = item;
 
       const { id_teacher: teacherID } = id_teacher;
       const classList = {
@@ -173,24 +146,6 @@ export class SchedulingService {
       );
       return a;
     }
-    dataSubject = filterData(dataSubject);
-    dataClass = filterData(dataClass);
-    dataTeacher = filterData(dataTeacher);
-    dataRoom = filterData(dataRoom);
-    dataGroup = filterData(dataGroup);
-    dataGroup = filterData(dataGroup);
-    const dataInput = {
-      class: dataClass,
-      prof: dataTeacher,
-      room: dataRoom,
-      group: dataGroup,
-      course: dataSubject,
-    };
-    const tmpData = {
-      username: 'abc',
-      password: '123456',
-    };
-    // tmpData = JSON.stringify(tmpData);
     const request = this.httpService
       .post('http://127.0.0.1:5000/scheduling', inputFinal)
       .pipe(map((res) => res.data))
@@ -210,7 +165,6 @@ export class SchedulingService {
       const filter = { _id: classItem.classID };
       const update = { status: true };
 
-      // `doc` is the document _before_ `update` was applied
       const _schedule = await this.scheduleModel.find({
         id_class: classItem.classID,
       });
